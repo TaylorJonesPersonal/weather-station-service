016c66ecab7719f87b13aba9ed180a3a79e19102
TaylorJonesPersonal <jonesftwingp@outlook.com>
2026-09-09 20:22:50 -0700

    enumerated wind direction

--- src/main/java/com/fujita/weather_station_service/Model/Reading.java
+++ src/main/java/com/fujita/weather_station_service/Model/Reading.java
@@ -7,17 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.Instant;
 
-@Entity
-@Table(name = "readings")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Reading {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
 
     private Instant createdAt = Instant.now();
     private float temperature;
